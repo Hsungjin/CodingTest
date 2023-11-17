b8376f2ccc5bd48ecdd4f071e6e4de6441dc9315
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2023-11-18 01:47:53 +0900

    [Bronze V] Title: A+B - 8, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.swift
@@ -0,0 +1,7 @@
+let t = Int(readLine()!)!
+
+for i in 1...t {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    print("Case #\(i): \(input[0]) + \(input[1]) = \(input[0] + input[1])")
+}
