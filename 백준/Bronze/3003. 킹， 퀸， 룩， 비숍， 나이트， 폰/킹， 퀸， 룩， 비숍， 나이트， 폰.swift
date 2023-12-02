8c2bc277ecf7e2856b9aba7f06d91245e0b4b5d4
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2023-12-02 17:41:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.swift
@@ -0,0 +1,6 @@
+var chass: [Int] = [1,1,2,2,2,8]
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+for i in 0..<input.count {
+    print(chass[i] - input[i], terminator: " ")
+}
