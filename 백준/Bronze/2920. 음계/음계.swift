6426d7f51b2a93ecb9f3ddf024aac6f5deb02fd8
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2023-12-17 00:39:35 +0900

    [Bronze II] Title: 음계, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.swift
@@ -0,0 +1,9 @@
+let input = readLine()!.split(separator: " ")
+
+if input == input.sorted() {
+    print("ascending")
+} else if input == input.sorted(by : >) {
+    print("descending")
+} else {
+    print("mixed")
+}
