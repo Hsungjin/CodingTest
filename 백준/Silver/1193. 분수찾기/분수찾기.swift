057179be1d90a511ce3fcb8d9867c212329acbb1
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2024-01-11 11:50:46 +0900

    [Silver V] Title: 분수찾기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.swift
@@ -0,0 +1,19 @@
+let input = readLine()!
+var N = Int(input)!
+var n : Int = 1
+
+while true {
+    let temp = N - n
+    if temp <= 0 {
+        if n % 2 == 0 {
+            print("\(N)/\((n - N + 1))")
+            break
+        } else {
+            print("\(n - N + 1)/\(N)")
+            break
+        }
+    } else if temp > 0 {
+        N = temp
+        n += 1
+    }
+}
