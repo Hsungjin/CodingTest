a14048d7ec8a3175097e4cbd4333a22014819579
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2024-02-20 10:37:32 +0900

    [Bronze V] Title: 팩토리얼, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.swift
@@ -0,0 +1,10 @@
+let input = Int(readLine()!)!
+
+func factorial(num: Int) -> Int {
+    if num == 0 {
+        return 1
+    }
+    return num * factorial(num: num - 1)
+}
+
+print(factorial(num: input))
