eea30a4c820c16469b3a06b4b2d0f972c1d93074
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2024-04-04 03:56:50 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ strlist:[String]) -> [Int] {
+    var result:[Int] = []
+    for i in 0..<strlist.count {
+        result.append(strlist[i].count)
+    }
+    return result
+}
