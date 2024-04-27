a272eb1a4ad60eb9466f35555aa0dc033f458ef0
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2024-04-27 15:13:06 +0900

    [Silver IV] Title: 수 찾기, Time: 224 ms, Memory: 80092 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -1,27 +1,28 @@
 let N = Int(readLine()!)!
-var input1 = readLine()!.split(separator: " ").map{ Int($0)! }
-input1.sort()
+var arr1 = readLine()!.split(separator: " ").map{ Int($0)! }
+arr1.sort()
 
 let M = Int(readLine()!)!
-let input2 = readLine()!.split(separator: " ").map{ Int($0)! }
+let arr2 = readLine()!.split(separator: " ").map{ Int($0)! }
 
-func binarySearch(_ arr: [Int], _ target: Int) -> Int{
+func binarySearch(arr: [Int], target: Int) -> Int {
     var start = 0
-    var end = arr.count - 1
-
+    var end = arr1.count - 1
+    
     while start <= end {
         let mid = (start + end) / 2
-            if arr[mid] == target {
-                return 1
-            }else if arr[mid] > target {
-                end = mid - 1
-            }else if arr[mid] < target {
-                start = mid + 1
-            }
+        if arr1[mid] == target {
+            return 1
+        } else if arr1[mid] > target {
+            end = mid - 1
+        } else if arr1[mid] < target {
+            start = mid + 1
         }
-        return 0
+    }
+    
+    return 0
 }
 
 for i in 0 ..< M {
-    print(binarySearch(input1, input2[i]))
+    print(binarySearch(arr: arr1, target: arr2[i]))
 }
