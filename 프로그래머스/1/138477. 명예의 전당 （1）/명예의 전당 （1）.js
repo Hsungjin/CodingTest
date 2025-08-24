ab9bd4de20d18aa0d6110b364a3a566ea5d9b89d
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2025-08-24 14:59:53 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.88 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,17 @@
+function solution(k, score) {
+    const answer = []
+    const result = []
+    
+    for(let i = 0 ; i < score.length ; i++) {
+        if(i < k) {
+            answer.push(score[i])
+        }
+        if(score[i]>Math.min(...answer)) {
+            answer.pop()
+            answer.push(score[i])
+            answer.sort((a, b) => b - a)
+        }
+        result.push(answer.at(-1))
+    }
+    return result
+}
