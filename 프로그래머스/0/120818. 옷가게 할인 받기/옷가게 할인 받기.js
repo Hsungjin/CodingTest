11aeada4efc868dd20ddffcaaff9db9b8dfd1c3f
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2025-08-25 15:59:06 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,4 @@
+function solution(price) {
+    return price < 100000 
+        ? price : price < 300000 ? Math.floor(price * 0.95) : price < 500000 ? Math.floor(price * 0.9) : Math.floor(price * 0.8);
+}
