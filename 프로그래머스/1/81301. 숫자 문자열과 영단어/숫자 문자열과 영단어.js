25b9cffb0b39373e32850e520eab11f114cf599e
Hsungjin <120264964+Hsungjin@users.noreply.github.com>
2025-08-31 16:49:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    const numberWords = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    };
+    
+    Object.keys(numberWords).forEach(word => {
+        s = s.replace(new RegExp(word, 'g'), numberWords[word]);
+    });
+    return Number(s);
+}
